32b4a315b70ae17cdaf27a26d0032ff860678437
Catie Rencricca <catierencricca@gmail.com>
2024-11-17 23:18:01 -0500

    Add responsiveness

--- src/components/TalkCard.tsx
+++ src/components/TalkCard.tsx
@@ -11,16 +11,16 @@ function TalkCard({ isDisplayed, video }: TalkCardProps) {
   const views = new Intl.NumberFormat('en-US', { notation: 'compact', maximumFractionDigits: 1 }).format(video.viewedCount)
   return (
     <article
-      className="object-contain rounded shadow hover:shadow-md transition-shadow overflow-hidden"
+      className="flex h-screen w-screen object-cover rounded shadow hover:shadow-md transition-shadow overflow-hidden"
       style={isDisplayed ? {} : { display: 'none' }}
     >
       <img
         src={imageURL}
         alt={video.title}
-        className="object-cover bg-center rounded"
+        className="w-full bg-cover bg-center rounded align-end"
         loading={!isDisplayed ? "lazy" : undefined}
       />
-      <div className="absolute w-full bottom-0 left-0 text-left bg-blue-800 px-4 py-2">
+      <div className="absolute w-full bottom-0 left-0 text-left bg-blue-800 px-4 py-2 ">
         <div className="flex flex-col gap-2">
           <h3 className="text-6xl font-bold mt-2 text-white uppercase">{video.title}</h3>
           <p className="text-2xl text-white">{video.presenterDisplayName}</p>
